d9d11f72daf37af0eec84ac9006aad5f92a93cdb
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2023-09-16 16:35:21 -0400

    Fixed a function name in the description

--- lua/entities/gmod_wire_expression2/core/custom/cl_npc_spawn.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_npc_spawn.lua
@@ -2,6 +2,6 @@ E2Helper.Descriptions["npcSpawn"] = [[
 Spawns an npc.
 ]]
 
-E2Helper.Descriptions["npcSetAng"] = [[
-Sets the angle of an npc.
+E2Helper.Descriptions["npcSetYaw"] = [[
+Sets the yaw of an npc.
 ]]
